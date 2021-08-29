3577fbf194f9c7673b0f8e62a46bea74b60f45e9
rsheppard83 <rsheppard83@gmail.com>
2021-08-29 14:57:57 +0100

    style component fix

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from 'react'
 import Image from 'next/image'
-import styles from '../styles/home.module.scss'
+import styles from '../styles/Home.module.scss'
 import gsap from 'gsap'
 
 const Hero = () => {
