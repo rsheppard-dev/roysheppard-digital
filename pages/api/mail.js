250349fc7d7078b3ba300953d7210f23fdba823c
rsheppard83 <rsheppard83@gmail.com>
2021-10-09 13:04:39 +0100

    debugging removed - api works :)

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -3,28 +3,24 @@ const mail = require('@sendgrid/mail')
 mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRIP_API_KEY)
 
 export default async (req, res) => {
-  if (process.env.NEXT_PUBLIC_SENDGRIP_API_KEY) {
-    console.log('api key seems to be present')
+  const body = JSON.parse(req.body)
+
+  const message = `
+  Name: ${body.name}\r\n
+  Email: ${body.email}\r\n
+  Phone: ${body.phone}\r\n
+  Message: ${body.message}
+`
+
+  const data = {
+    to: 'info@roysheppard.digital',
+    from: body.email,
+    subject: 'Customer Enquiry',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>')
   }
 
   try {
-    const body = JSON.parse(req.body)
-
-    const message = `
-    Name: ${body.name}\r\n
-    Email: ${body.email}\r\n
-    Phone: ${body.phone}\r\n
-    Message: ${body.message}
-  `
-
-    const data = {
-      to: 'info@roysheppard.digital',
-      from: body.email,
-      subject: 'Customer Enquiry',
-      text: message,
-      html: message.replace(/\r\n/g, '<br>')
-    }
-
     await mail.send(data)
 
     res.status(200).json({ status: 'ok' })
