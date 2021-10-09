e710a86c109f5970967c0ac8ddcc8bb2075e92a9
rsheppard83 <rsheppard83@gmail.com>
2021-10-09 12:59:48 +0100

    added api debugging logs

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,25 +2,33 @@ const mail = require('@sendgrid/mail')
 
 mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRIP_API_KEY)
 
-export default (req, res) => {
-  const body = JSON.parse(req.body)
-  
-  const message = `
+export default async (req, res) => {
+  if (process.env.NEXT_PUBLIC_SENDGRIP_API_KEY) {
+    console.log('api key seems to be present')
+  }
+
+  try {
+    const body = JSON.parse(req.body)
+
+    const message = `
     Name: ${body.name}\r\n
     Email: ${body.email}\r\n
     Phone: ${body.phone}\r\n
     Message: ${body.message}
   `
 
-  const data = {
-    to: 'info@roysheppard.digital',
-    from: body.email,
-    subject: 'Customer Enquiry',
-    text: message,
-    html: message.replace(/\r\n/g, '<br>')
+    const data = {
+      to: 'info@roysheppard.digital',
+      from: body.email,
+      subject: 'Customer Enquiry',
+      text: message,
+      html: message.replace(/\r\n/g, '<br>')
+    }
+
+    await mail.send(data)
+
+    res.status(200).json({ status: 'ok' })
+  } catch (error) {
+    console.log(error)
   }
-
-  mail.send(data)
-
-  res.status(200).json({ status: 'ok' })
 }
