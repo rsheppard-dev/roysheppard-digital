828a7c4d7eb80e706985ad799a9b4d2950a4f7fa
Roy Sheppard <70776124+rsheppard-dev@users.noreply.github.com>
2021-10-25 21:45:34 +0100

    Update About.js
    Fixed typo

--- components/About.js
+++ components/About.js
@@ -11,7 +11,7 @@ const About = () => {
 
       <p>My name is Roy Sheppard and I am a freelance web designer and full-stack web developer based in Watford, Hertfordshire.</p>
 
-      <p>I take pride in creating engaging websites that don't just look good, but are fast, responsive and convert vistors into customers.</p>
+      <p>I take pride in creating engaging websites that don't just look good, but are fast, responsive and convert visitors into customers.</p>
 
       <p>I would love to work with you to find out what you want to get out of a website and come up with solutions to drive more customers to your business.</p>
 
