320a50e217376483cbe84ea3fcdc451cc1c0b167
Roy Sheppard <rsheppard83@gmail.com>
2021-11-12 21:51:17 +0000

    web design features

--- pages/web-design.js
+++ pages/web-design.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 import Contact from '../components/Contact'
@@ -20,43 +21,70 @@ const WebDesign = () => {
 
                     <div className={styles.quoteBox}>
                         <FontAwesomeIcon icon={faQuoteLeft} className={styles.quoteIcon} />
-                        <p>
                             Website design is an important factor for determining credibility for 48% of visitors
-                        </p>
                     </div>
 
-                    <p>I don't just make websites. I make websites that are tailored to you and your business.</p>
+                    <p>I design websites that are tailor made with your business in mind. All my websites are included with the following as standard…</p>
 
-                    <div className="row row-cols-1 row-cols-md-2 g-4">
-                        <div className="col">
-                            <div className={`${styles.card} card h-100`}>
-                                <div className="card-body">
-                                    <span className={styles.cardNumber}>1</span>
-                                    <h3 className="card-title subHeading">Responsiveness</h3>
-                                    <p className="card-text">All my websites are designed to work on different devices; from a mobile phone, to a tablet to the latest 4K monitor.</p>
-                                </div>
-                            </div>
+                    <div className={styles.featuresBox}>
+                        <div className={styles.featuresHeader}>
+                            <Image
+                                src="/images/lightning.png"
+                                alt="bullet point"
+                                width={60}
+                                height={60}
+                            />
+                            <h3>UI/UX web design</h3>
                         </div>
-
-                        <div className="col">
-                            <div className={`${styles.card} card h-100`}>
-                                <div className="card-body">
-                                    <span className={styles.cardNumber}>2</span>
-                                    <h3 className="card-title subHeading">Accessibility</h3>
-                                    <p className="card-text">My websites are made to be accessibile for all users. Care is taken to ensure users with disabilities can still easily navigate and use your website.</p>
-                                </div>
-                            </div>
+                        <p>Understanding user experience and user interface design are import concepts when designing any website but especially when working with business websites.</p>
+                    </div>
+                    <div className={styles.featuresBox}>
+                        <div className={styles.featuresHeader}>
+                            <Image
+                                src="/images/lightning.png"
+                                alt="bullet point"
+                                width={60}
+                                height={60}
+                            />
+                            <h3>Responsive web design</h3>
                         </div>
-
-                        <div className="col">
-                            <div className={`${styles.card} card h-100`}>
-                                <div className="card-body">
-                                    <span className={styles.cardNumber}>3</span>
-                                    <h3 className="card-title subHeading">SEO</h3>
-                                    <p className="card-text">There's no point having an amazing website if no one can find it. All websites made by me are optimised to stand the best chance of being ranked by search engines.</p>
-                                </div>
-                            </div>
+                        <p>My websites are made to respond to whatever device your users choose to use, whether that’s a mobile, tablet, laptop or a 4K monitor.</p>
+                    </div>
+                    <div className={styles.featuresBox}>
+                        <div className={styles.featuresHeader}>
+                            <Image
+                                src="/images/lightning.png"
+                                alt="bullet point"
+                                width={60}
+                                height={60}
+                            />
+                            <h3>Accessibility</h3>
                         </div>
+                        <p>People with different disabilities may use your site and it is important their experience is a positive one. My sites use best practice methods to ensure these user can still navigate your website successfully.</p>
+                    </div>
+                    <div className={styles.featuresBox}>
+                        <div className={styles.featuresHeader}>
+                            <Image
+                                src="/images/lightning.png"
+                                alt="bullet point"
+                                width={60}
+                                height={60}
+                            />
+                            <h3>SEO optimisation</h3>
+                        </div>
+                        <p>There is no point having an amazing website if no-one can find it. My websites are optimised to stand the best chance of being found by your intended users.</p>
+                    </div>
+                    <div className={styles.featuresBox}>
+                        <div className={styles.featuresHeader}>
+                            <Image
+                                src="/images/lightning.png"
+                                alt="bullet point"
+                                width={60}
+                                height={60}
+                            />
+                            <h3>Google analytics</h3>
+                        </div>
+                        <p>Analytics data helps you learn who is visiting your site and how they are using it to help you improve your users’ experience as well as improving your conversions and sales.</p>
                     </div>
                 </section>
                 
