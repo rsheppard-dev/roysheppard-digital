24d2b8d82d6da0620d6a42e4093bf299a3542ad6
Roy Sheppard <rsheppard83@gmail.com>
2021-11-19 00:07:08 +0000

    added open graph tags

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,11 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="robots" content="index, follow" />
+                    <meta property="og:title" content="Roy Sheppard - Freelance Web Designer + Developer" />
+                    <meta property="og:url" content="https://www.roysheppard.digital/web-design-watford" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:description" content="Are you looking to increase traffic to your website? Or are you trying get your business online? My name is Roy Sheppard and I am a freelance web designer and developer based in Watford. Click the link to find out more about my services." />
+                    <meta property="og:image" content="https://www.roysheppard.digital/images/og.jpg" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
