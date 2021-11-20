97da3bd5b798c6f1d89fb937b1cd3d62ea98818b
Roy Sheppard <rsheppard83@gmail.com>
2021-11-20 05:45:22 +0000

    og image change

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
                     <meta property="og:url" content="https://www.roysheppard.digital/web-design-watford" />
                     <meta property="og:type" content="website" />
                     <meta property="og:description" content="Are you looking to increase traffic to your website? Or are you trying get your business online? My name is Roy Sheppard and I am a freelance web designer and developer based in Watford. Click the link to find out more about my services." />
-                    <meta property="og:image" content="https://www.roysheppard.digital/images/og-fb.jpg" />
+                    <meta property="og:image" content="https://www.roysheppard.digital/images/og.png" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
