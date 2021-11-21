8a68be98d9ec18ba1a9b8c99cd655c3f06c3b22a
Roy Sheppard <rsheppard83@gmail.com>
2021-11-21 18:58:15 +0000

    ecommerce page updated

--- pages/ecommerce.js
+++ pages/ecommerce.js
@@ -1,25 +1,75 @@
-import Head from 'next/head'
-import Contact from '../components/Contact';
-import animateUnderline from '../utils/animateUnderline'
+import Head from "next/head";
+import Link from "next/link";
+import { FaQuoteLeft, FaCcStripe, FaCcPaypal } from "react-icons/fa";
+import { SiShopify, SiWoo } from 'react-icons/si'
+import Contact from "../components/Contact";
+import animateUnderline from "../utils/animateUnderline";
+import styles from "../styles/Services.module.scss";
 
 const Ecommerce = () => {
-    animateUnderline()
+  animateUnderline();
 
-    return (
-        <>
-            <Head>
-                <title>eCommerce Specialist Watford - Roy Sheppard Digital</title>
-                <meta name="description" content="Freelance eCommerce specialist in Watford, Hertfordshire. Helping businesses grow online." />
-            </Head>
-            <main>
-                <section className="container">
-                    <h1 className="heading"><span className="underline">eCommerce</span></h1>
-                </section>
-                
-                <Contact />
-            </main>
-        </>
-    );
-}
+  return (
+    <>
+      <Head>
+        <title>eCommerce Specialist Watford - Roy Sheppard Digital</title>
+        <meta
+          name="description"
+          content="Freelance eCommerce specialist in Watford, Hertfordshire. Helping businesses grow online."
+        />
+      </Head>
+      <main>
+        <section className="container">
+          <h1 className="heading">
+            Using <span className="underline">eCommerce</span> to future proof
+            your business
+          </h1>
+
+          <div className={styles.quoteBox}>
+            <FaQuoteLeft className={styles.quoteIcon} />
+            60% of people said that usability is the most important web design
+            characteristic when it comes to online shopping
+          </div>
+
+          <p>
+            There are many options when deciding to start an ecommerce business
+            or when extending an existing business to include online sales. I
+            can help you decide what is the best option for you and your target
+            market.
+          </p>
+
+          <Link href="ecommerce/#contact-section">
+            <a className="button button-large">Ready to get started?</a>
+          </Link>
+
+          <h2 className="heading">
+            <span className="underline">eCommerce</span> tools
+          </h2>
+          <p>
+            From selecting what platform will work best with your business to
+            selecting payment processors. I am passionate about helping new and
+            existing businesses sell their products and services online.
+          </p>
+          <div className={styles.skillGrid}>
+            <div className={styles.skill}>
+              <SiShopify size={60} color="#95BF46" />
+            </div>
+            <div className={styles.skill}>
+              <SiWoo size={60} color="#7B51AD" />
+            </div>
+            <div className={styles.skill}>
+              <FaCcStripe size={60} color="#6771E5" />
+            </div>
+            <div className={styles.skill}>
+              <FaCcPaypal size={60} color="#012069" />
+            </div>
+          </div>
+        </section>
+
+        <Contact />
+      </main>
+    </>
+  );
+};
 
 export default Ecommerce;
--- pages/web-development.js
+++ pages/web-development.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 import {
   SiHtml5,
   SiCss3,
@@ -14,8 +13,9 @@ import {
   SiNodedotjs,
   SiMongodb,
   SiPhp,
-  SiGraphql
+  SiGraphql,
 } from "react-icons/si";
+import { FaQuoteLeft } from "react-icons/fa";
 import animateUnderline from "../utils/animateUnderline";
 import Contact from "../components/Contact";
 import styles from "../styles/Services.module.scss";
@@ -40,8 +40,8 @@ const WebDevelopment = () => {
           </h1>
 
           <div className={styles.quoteBox}>
-            <FontAwesomeIcon icon={faQuoteLeft} className={styles.quoteIcon} />
-            42% of people will leave a website because of poor functionality.
+            <FaQuoteLeft className={styles.quoteIcon} />
+            42% of people will leave a website because of poor functionality
           </div>
 
           <p className={styles.statement}>
@@ -51,51 +51,60 @@ const WebDevelopment = () => {
             giving you back your time to focus on your business.
           </p>
 
+          <Link href="web-development/#contact-section">
+            <a className="button button-large">Ready to get started?</a>
+          </Link>
+
           <h2 className="heading">
             <span className="underline">Web development</span> skills
           </h2>
 
-          <p className={styles.statement}>I am an experienced full-stack developer who has worked with many technologies, including modern JAMstack tools. I beleive in using the right tool for the job, depending on your specific needs. Here are a handful of the development tools I work with...</p>
+          <p className={styles.statement}>
+            I am an experienced full-stack developer who has worked with many
+            tech stacks, including the modern JAMstack. I beleive in using the
+            right tools for the job depending on your specific needs. Here are a
+            handful of the development tools I work with...
+          </p>
 
           <div className={styles.skillGrid}>
             <div className={styles.skill}>
-              <SiHtml5 size={60} />
+              <SiHtml5 size={60} color="#E44D27" />
             </div>
             <div className={styles.skill}>
-              <SiCss3 size={60} />
+              <SiCss3 size={60} color="#205EA4" />
             </div>
             <div className={styles.skill}>
-              <SiSass size={60} />
+              <SiSass size={60} color="#C16291" />
             </div>
             <div className={styles.skill}>
-              <SiBootstrap size={60} />
+              <SiBootstrap size={60} color="#503A74" />
             </div>
             <div className={styles.skill}>
-              <SiTailwindcss size={60} />
+              <SiTailwindcss size={60} color="#18B3C0" />
             </div>
             <div className={styles.skill}>
-              <SiJavascript size={60} />
+              <SiJavascript size={60} color="#EBD41B" />
             </div>
             <div className={styles.skill}>
-              <SiReact size={60} />
+              <SiReact size={60} color="#5CCEED" />
             </div>
             <div className={styles.skill}>
-              <SiNextdotjs size={60} />
+              <SiNextdotjs size={60} color="#000000" />
             </div>
             <div className={styles.skill}>
-              <SiGraphql size={60} />
+              <SiGraphql size={60} color="#D92FA2" />
             </div>
             <div className={styles.skill}>
-              <SiWordpress size={60} />
+              <SiWordpress size={60} color="#1F6E93" />
             </div>
             <div className={styles.skill}>
-              <SiNodedotjs size={60} />
+              <SiNodedotjs size={60} color="#62975E" />
             </div>
             <div className={styles.skill}>
-              <SiMongodb size={60} />
+              <SiMongodb size={60} color="#11A14B" />
             </div>
             <div className={styles.skill}>
-              <SiPhp size={60} />
+              <SiPhp size={60} color="#7175AA" />
             </div>
           </div>
         </section>
