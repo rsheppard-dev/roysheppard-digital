dbb2404fce2efd2495a30341bea10af2f28b7b08
Roy Sheppard <rsheppard83@gmail.com>
2021-12-04 22:57:28 +0000

    updated meta desc

--- pages/ecommerce.js
+++ pages/ecommerce.js
@@ -1,74 +1,74 @@
-import Head from "next/head";
-import Link from "next/link";
-import { FaCcStripe, FaCcPaypal } from "react-icons/fa";
-import { SiShopify, SiWoo } from 'react-icons/si'
-import Contact from "../components/Contact";
-import animateUnderline from "../utils/animateUnderline";
-import styles from "../styles/Services.module.scss";
+import Head from 'next/head';
+import Link from 'next/link';
+import { FaCcStripe, FaCcPaypal } from 'react-icons/fa';
+import { SiShopify, SiWoo } from 'react-icons/si';
+import Contact from '../components/Contact';
+import animateUnderline from '../utils/animateUnderline';
+import styles from '../styles/Services.module.scss';
 
 const Ecommerce = () => {
-  animateUnderline();
+	animateUnderline();
 
-  return (
-    <>
-      <Head>
-        <title>eCommerce Specialist Watford - Roy Sheppard Digital</title>
-        <meta
-          name="description"
-          content="I am Roy Sheppard, a freelance eCommerce specialist from Watford. I help businesses grow by working with them to get them selling online."
-        />
-      </Head>
-      <main>
-        <section className="container">
-          <h1 className="heading">
-            Using <span className="underline">eCommerce</span> to future proof
-            your business
-          </h1>
+	return (
+		<>
+			<Head>
+				<title>eCommerce Specialist Watford - Roy Sheppard Digital</title>
+				<meta
+					name='description'
+					content='Get in touch today for a FREE ecommerce strategy call. I am Roy Sheppard, a freelance web designer and developer from Watford.'
+				/>
+			</Head>
+			<main>
+				<section className='container'>
+					<h1 className='heading'>
+						Using <span className='underline'>eCommerce</span> to future proof
+						your business
+					</h1>
 
-          <div className={styles.quoteBox}>
-            60% of people said that usability is the most important web design
-            characteristic when it comes to online shopping
-          </div>
+					<div className={styles.quoteBox}>
+						60% of people said that usability is the most important web design
+						characteristic when it comes to online shopping
+					</div>
 
-          <p>
-            There are many options when deciding to start an ecommerce business
-            or when extending an existing business to include online sales. I
-            can help you decide what is the best option for you and your target
-            market.
-          </p>
+					<p>
+						There are many options when deciding to start an ecommerce business
+						or when extending an existing business to include online sales. I
+						can help you decide what is the best option for you and your target
+						market.
+					</p>
 
-          <Link href="/ecommerce#contact-section">
-            <a className="button button-large">Ready to get started?</a>
-          </Link>
+					<Link href='/ecommerce#contact-section'>
+						<a className='button button-large'>Ready to get started?</a>
+					</Link>
 
-          <h2 className="heading">
-            <span className="underline">eCommerce</span> tools
-          </h2>
-          <p>
-            From selecting what platform will work best with your business to
-            selecting payment processors. I am passionate about helping new and
-            existing businesses sell their products and services online.
-          </p>
-          <div className={styles.skillGrid}>
-            <div className={styles.skill}>
-              <SiShopify size={60} color="#95BF46" />
-            </div>
-            <div className={styles.skill}>
-              <SiWoo size={60} color="#7B51AD" />
-            </div>
-            <div className={styles.skill}>
-              <FaCcStripe size={60} color="#6771E5" />
-            </div>
-            <div className={styles.skill}>
-              <FaCcPaypal size={60} color="#012069" />
-            </div>
-          </div>
-        </section>
+					<h2 className='heading'>
+						<span className='underline'>eCommerce</span> tools
+					</h2>
+					<p>
+						From selecting what platform will work best with your business to
+						selecting payment processors. I am passionate about helping new and
+						existing businesses sell their products and services online.
+					</p>
+					<div className={styles.skillGrid}>
+						<div className={styles.skill}>
+							<SiShopify size={60} color='#95BF46' />
+						</div>
+						<div className={styles.skill}>
+							<SiWoo size={60} color='#7B51AD' />
+						</div>
+						<div className={styles.skill}>
+							<FaCcStripe size={60} color='#6771E5' />
+						</div>
+						<div className={styles.skill}>
+							<FaCcPaypal size={60} color='#012069' />
+						</div>
+					</div>
+				</section>
 
-        <Contact />
-      </main>
-    </>
-  );
+				<Contact />
+			</main>
+		</>
+	);
 };
 
 export default Ecommerce;
--- pages/web-development.js
+++ pages/web-development.js
@@ -1,115 +1,115 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 import {
-  SiHtml5,
-  SiCss3,
-  SiSass,
-  SiBootstrap,
-  SiTailwindcss,
-  SiJavascript,
-  SiReact,
-  SiNextdotjs,
-  SiWordpress,
-  SiNodedotjs,
-  SiMongodb,
-  SiPhp,
-  SiGraphql,
-} from "react-icons/si";
-import animateUnderline from "../utils/animateUnderline";
-import Contact from "../components/Contact";
-import styles from "../styles/Services.module.scss";
+	SiHtml5,
+	SiCss3,
+	SiSass,
+	SiBootstrap,
+	SiTailwindcss,
+	SiJavascript,
+	SiReact,
+	SiNextdotjs,
+	SiWordpress,
+	SiNodedotjs,
+	SiMongodb,
+	SiPhp,
+	SiGraphql,
+} from 'react-icons/si';
+import animateUnderline from '../utils/animateUnderline';
+import Contact from '../components/Contact';
+import styles from '../styles/Services.module.scss';
 
 const WebDevelopment = () => {
-  animateUnderline();
+	animateUnderline();
 
-  return (
-    <>
-      <Head>
-        <title>Web Developer Watford - Roy Sheppard Digital</title>
-        <meta
-          name="description"
-          content="I am Roy Sheppard, a freelance web developer from Watford. I make websites that are interactive and help solve your businesses specific needs."
-        />
-      </Head>
-      <main>
-        <section className="container">
-          <h1 className="heading">
-            <span className="underline">Web development</span> that makes
-            running your business easier
-          </h1>
+	return (
+		<>
+			<Head>
+				<title>Web Developer Watford - Roy Sheppard Digital</title>
+				<meta
+					name='description'
+					content='Get in touch today for a FREE web development strategy call. I am Roy Sheppard, a freelance web designer and developer from Watford.'
+				/>
+			</Head>
+			<main>
+				<section className='container'>
+					<h1 className='heading'>
+						<span className='underline'>Web development</span> that makes
+						running your business easier
+					</h1>
 
-          <div className={styles.quoteBox}>
-            42% of people will leave a website because of poor functionality
-          </div>
+					<div className={styles.quoteBox}>
+						42% of people will leave a website because of poor functionality
+					</div>
 
-          <p className={styles.statement}>
-            I specialise in making bespoke functionality for your website. I
-            understand that every business is unique and has unique needs. A
-            good developer can come up with solutions that solve problems -
-            giving you back your time to focus on your business.
-          </p>
+					<p className={styles.statement}>
+						I specialise in making bespoke functionality for your website. I
+						understand that every business is unique and has unique needs. A
+						good developer can come up with solutions that solve problems -
+						giving you back your time to focus on your business.
+					</p>
 
-          <Link href="/web-development#contact-section">
-            <a className="button button-large">Ready to get started?</a>
-          </Link>
+					<Link href='/web-development#contact-section'>
+						<a className='button button-large'>Ready to get started?</a>
+					</Link>
 
-          <h2 className="heading">
-            <span className="underline">Web development</span> skills
-          </h2>
+					<h2 className='heading'>
+						<span className='underline'>Web development</span> skills
+					</h2>
 
-          <p className={styles.statement}>
-            I am an experienced full-stack developer who has worked with many
-            tech stacks, including the modern JAMstack. I beleive in using the
-            right tools for the job depending on your specific needs. Here are a
-            handful of the development tools I work with...
-          </p>
+					<p className={styles.statement}>
+						I am an experienced full-stack developer who has worked with many
+						tech stacks, including the modern JAMstack. I beleive in using the
+						right tools for the job depending on your specific needs. Here are a
+						handful of the development tools I work with...
+					</p>
 
-          <div className={styles.skillGrid}>
-            <div className={styles.skill}>
-              <SiHtml5 size={60} color="#E44D27" />
-            </div>
-            <div className={styles.skill}>
-              <SiCss3 size={60} color="#205EA4" />
-            </div>
-            <div className={styles.skill}>
-              <SiSass size={60} color="#C16291" />
-            </div>
-            <div className={styles.skill}>
-              <SiBootstrap size={60} color="#503A74" />
-            </div>
-            <div className={styles.skill}>
-              <SiTailwindcss size={60} color="#18B3C0" />
-            </div>
-            <div className={styles.skill}>
-              <SiJavascript size={60} color="#EBD41B" />
-            </div>
-            <div className={styles.skill}>
-              <SiReact size={60} color="#5CCEED" />
-            </div>
-            <div className={styles.skill}>
-              <SiNextdotjs size={60} color="#000000" />
-            </div>
-            <div className={styles.skill}>
-              <SiGraphql size={60} color="#D92FA2" />
-            </div>
-            <div className={styles.skill}>
-              <SiWordpress size={60} color="#1F6E93" />
-            </div>
-            <div className={styles.skill}>
-              <SiNodedotjs size={60} color="#62975E" />
-            </div>
-            <div className={styles.skill}>
-              <SiMongodb size={60} color="#11A14B" />
-            </div>
-            <div className={styles.skill}>
-              <SiPhp size={60} color="#7175AA" />
-            </div>
-          </div>
-        </section>
-        <Contact />
-      </main>
-    </>
-  );
+					<div className={styles.skillGrid}>
+						<div className={styles.skill}>
+							<SiHtml5 size={60} color='#E44D27' />
+						</div>
+						<div className={styles.skill}>
+							<SiCss3 size={60} color='#205EA4' />
+						</div>
+						<div className={styles.skill}>
+							<SiSass size={60} color='#C16291' />
+						</div>
+						<div className={styles.skill}>
+							<SiBootstrap size={60} color='#503A74' />
+						</div>
+						<div className={styles.skill}>
+							<SiTailwindcss size={60} color='#18B3C0' />
+						</div>
+						<div className={styles.skill}>
+							<SiJavascript size={60} color='#EBD41B' />
+						</div>
+						<div className={styles.skill}>
+							<SiReact size={60} color='#5CCEED' />
+						</div>
+						<div className={styles.skill}>
+							<SiNextdotjs size={60} color='#000000' />
+						</div>
+						<div className={styles.skill}>
+							<SiGraphql size={60} color='#D92FA2' />
+						</div>
+						<div className={styles.skill}>
+							<SiWordpress size={60} color='#1F6E93' />
+						</div>
+						<div className={styles.skill}>
+							<SiNodedotjs size={60} color='#62975E' />
+						</div>
+						<div className={styles.skill}>
+							<SiMongodb size={60} color='#11A14B' />
+						</div>
+						<div className={styles.skill}>
+							<SiPhp size={60} color='#7175AA' />
+						</div>
+					</div>
+				</section>
+				<Contact />
+			</main>
+		</>
+	);
 };
 
 export default WebDevelopment;
