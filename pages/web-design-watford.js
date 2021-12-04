2558aea47166bf4c469104ad7b139c1428c93b7c
Roy Sheppard <rsheppard83@gmail.com>
2021-12-04 22:54:06 +0000

    updated meta desc

--- pages/web-design-watford.js
+++ pages/web-design-watford.js
@@ -1,40 +1,42 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import Services from '../components/Services'
-import Faq from '../components/Faq'
-import Contact from '../components/Contact'
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Services from '../components/Services';
+import Faq from '../components/Faq';
+import Contact from '../components/Contact';
 
 export default function Home({ faqs }) {
+	return (
+		<>
+			<Head>
+				<title>Web Design Watford - Roy Sheppard Digital</title>
+				<meta
+					name='description'
+					content='Get in touch today for a FREE website strategy call. I am Roy Sheppard, a freelance web designer and developer from Watford.'
+				/>
+			</Head>
 
-  return (
-    <>
-      <Head>
-        <title>Web Design Watford - Roy Sheppard Digital</title>
-        <meta name="description" content="I am Roy Sheppard, a freelance web designer and developer from Watford. I specialise in creating fast, engaging websites that get results." />
-      </Head>
+			<Hero />
 
-      <Hero />
+			<About />
 
-      <About />
+			<Services />
 
-      <Services />
-      
-      <Faq faqs={faqs} />
+			<Faq faqs={faqs} />
 
-      <Contact />
-    </>
-  )
+			<Contact />
+		</>
+	);
 }
 
 export async function getStaticProps() {
-  const uri = process.env.NEXT_PUBLIC_WP_GRAPHQL
-  const res = await fetch(uri, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-          query: `
+	const uri = process.env.NEXT_PUBLIC_WP_GRAPHQL;
+	const res = await fetch(uri, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			query: `
               query faqQuery {
                 faqs {
                   nodes {
@@ -44,15 +46,15 @@ export async function getStaticProps() {
                   }
                 }
               }
-          `
-      })
-  })
+          `,
+		}),
+	});
 
-  const json = await res.json()
+	const json = await res.json();
 
-  return {
-      props: {
-          faqs: json.data.faqs
-      }
-  }
+	return {
+		props: {
+			faqs: json.data.faqs,
+		},
+	};
 }
