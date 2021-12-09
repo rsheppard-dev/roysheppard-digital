7601e3019165f16607243111cf204f7ef6f6a5bb
Roy Sheppard <rsheppard83@gmail.com>
2021-12-09 22:10:04 +0000

    removed hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -63,19 +63,6 @@ class MyDocument extends Document {
 						href='/favicon-16x16.png'
 					/>
 					<link rel='manifest' href='/site.webmanifest' />
-
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                            h._hjSettings={hjid:2733262,hjsv:6};
-                            a=o.getElementsByTagName('head')[0];
-                            r=o.createElement('script');
-                            r.async=1;
-                            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                            a.appendChild(r);
-                            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-						}}
-					/>
 				</Head>
 				<body>
 					<noscript>
